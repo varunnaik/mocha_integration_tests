9685f92dd4206319b2217394362f4f44986831e5
Varun Naik <varun@homescript.com.au>
2017-09-15 15:07:21 +1000

    Add a very basic rest api

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+var users = [];
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json(users);
 });
+
+router.post('/', function(req, res) {
+  users.push(req.body);
+  res.sendStatus(200);
+})
+
+router.delete('/:id', function(req,res) {
+  users = users.filter(u => u.id !== req.params.id)
+  res.sendStatus(200);
+})
+
+router.get('/:id', function(req, res) {
+  res.json(users.filter(u => u.id === req.params.id));
+})
 
 module.exports = router;
